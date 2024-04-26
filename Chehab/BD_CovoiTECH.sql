11701c2c8db483a7a82d1bf1a76bdc19b45d108a
miniar <123066240+miniarjabri@users.noreply.github.com>
2024-04-26 20:05:24 +0200

    Update BD_CovoiTECH.sql

--- Chehab/BD_CovoiTECH.sql
+++ Chehab/BD_CovoiTECH.sql
@@ -1,13 +1,17 @@
-CREATE TABLE Messagerie(
-   IdSession INT,
-   Message VARCHAR(50),
-   PRIMARY KEY(IdSession)
+
+--création de la table messagerie -- testé et c'est bon 
+CREATE TABLE Messagerie (
+    IdSession INT ,
+    Message TEXT,
+    PRIMARY KEY(IdSession)
 );
+--création de la table visiteur -- testé et c'est bon 
 
 CREATE TABLE Visiteur(
    IdTemp VARCHAR(50),
    PRIMARY KEY(IdTemp)
 );
+--création de la table admin  -- testé et c'est bon 
 
 CREATE TABLE Administrateur(
    IdAdm INT,
@@ -16,108 +20,124 @@ CREATE TABLE Administrateur(
    PRIMARY KEY(IdAdm)
 );
 
+--la table Date-- testé et c'est bon 
 CREATE TABLE LaDate(
-   Jour BYTE,
-   Mois SMALLINT,
-   Annee INT,
-   Heure VARCHAR(50),
+    Jour INT CHECK (Jour BETWEEN 1 AND 31),
+    Mois INT CHECK (Mois BETWEEN 1 AND 12),
+    Annee INT CHECK (Annee > 0),
+    Heure TIME ,
    PRIMARY KEY(Jour, Mois, Annee)
 );
+--la table Calendrier-- testé et c'est bon 
 
 CREATE TABLE Calendrier(
    HeureDepart TIME,
    PRIMARY KEY(HeureDepart)
 );
 
+--la table jour -- testé et c'est bon 
 CREATE TABLE Jour(
-   JourDépart DATE,
-   JourArrivée DATE,
-   Semaine INT,
-   PRIMARY KEY(JourDépart, JourArrivée, Semaine)
+  JourDepart INT NOT NULL CHECK (JourDepart BETWEEN 1 AND 7),
+   JourArrivee INT NOT NULL CHECK (JourArrivee BETWEEN 1 AND 7),
+   Semaine INT NOT NULL CHECK (Semaine BETWEEN 1 AND 52),
+   PRIMARY KEY(JourDepart, JourArrivee, Semaine)
 );
 
+--la table boutique -- testé et c'est bon 
+
 CREATE TABLE Boutique(
-   IdCadeau VARCHAR(50),
-   NomCadeau VARCHAR(50),
-   DescriptionCadeau VARCHAR(50),
-   PointsNécessaires VARCHAR(50),
+   IdCadeau INT,
+   NomCadeau VARCHAR(100),
+   DescriptionCadeau TEXT,
+   PointsNecessaires INT CHECK (PointsNecessaires >= 0),
    PRIMARY KEY(IdCadeau)
 );
+--la table Escale -- testé et c'est bon 
 
 CREATE TABLE Escale(
    IdLieu VARCHAR(50),
-   NomRue VARCHAR(50),
-   NumRue VARCHAR(50),
-   CodePostal VARCHAR(50),
-   Accessibilté VARCHAR(50),
+   NomRue VARCHAR(100) NOT NULL CHECK (NomRue ~ '^[A-Za-z ]+$'),
+    NumRue INT NOT NULL CHECK (NumRue BETWEEN 1 AND 9999),
+    CodePostal VARCHAR(5) NOT NULL CHECK (CodePostal ~ '^\d{5}$'),
+    Accessibilite BOOLEAN NOT NULL,
    PRIMARY KEY(IdLieu)
 );
+--la table Historique -- testé et c'est bon 
 
 CREATE TABLE Historique(
-   IdHistorique VARCHAR(50),
+   IdHistorique INT,
    PRIMARY KEY(IdHistorique)
 );
 
+
+--création de la table utilisateur --  testé et c'est bon
+
 CREATE TABLE Utilisateur(
-   IdUtilisateur INT,
-   Mail VARCHAR(50),
-   MotdePasse VARCHAR(50),
-   NomUtilisateur VARCHAR(50),
-   PrenomUtilisateur VARCHAR(50),
-   NotePassager VARCHAR(50),
-   LangueParlée1 VARCHAR(50),
-   LangueParlée2 VARCHAR(50),
-   Sexe LOGICAL,
-   Tel INT,
-   Fumeur VARCHAR(50),
-   Handicap LOGICAL,
-   Jour BYTE NOT NULL,
-   Mois SMALLINT NOT NULL,
-   Annee INT NOT NULL,
+    IdUtilisateur INT,
+    Mail VARCHAR(100) UNIQUE NOT NULL CHECK (Mail LIKE '%@%.%'),
+    MotDePasse VARCHAR(100) NOT NULL CHECK (MotDePasse ~ '.*[a-z]+.*' AND MotDePasse ~ '.*[A-Z]+.*'),
+    Nom VARCHAR(100) NOT NULL CHECK (Nom ~ '^[A-Za-z]+$'),
+    Prenom VARCHAR(100) NOT NULL CHECK (Prenom ~ '^[A-Za-z]+$'),
+    Sexe CHAR(1) NOT NULL CHECK (Sexe IN ('M', 'F')), 
+    Tel VARCHAR(100) NOT NULL CHECK (length(Tel) = 8 AND Tel ~ '^[0-9]+$' AND Tel LIKE '0%'),
+    Handicap BOOLEAN NOT NULL,
+    NotePassager INT CHECK (NotePassager BETWEEN 0 AND 5),
+    LangueParle1 VARCHAR(50) NOT NULL CHECK (LangueParle1 ~ '^[A-Za-z]+$'),
+    LangueParle2 VARCHAR(50) CHECK (LangueParle2 ~ '^[A-Za-z]*$'),
+    Fumeur BOOLEAN NOT NULL, 
+	Jour INT CHECK (Jour BETWEEN 1 AND 31) NOT NULL,
+    Mois INT CHECK (Mois BETWEEN 1 AND 12) NOT NULL,
+    Annee INT CHECK (Annee > 0)NOT NULL,
    PRIMARY KEY(IdUtilisateur),
    FOREIGN KEY(Jour, Mois, Annee) REFERENCES LaDate(Jour, Mois, Annee)
 );
 
+--création de la table conducteur --  testé et c'est bon
+
 CREATE TABLE Conducteur(
-   NumPermis VARCHAR(50),
-   Points VARCHAR(50),
-   NoteConducteur VARCHAR(50),
+   NumPermis VARCHAR(50) NOT NULL,
+   Points INT CHECK (Points BETWEEN 0 AND 15),
+   NoteConducteur FLOAT,
    IdUtilisateur INT NOT NULL,
    PRIMARY KEY(NumPermis),
    FOREIGN KEY(IdUtilisateur) REFERENCES Utilisateur(IdUtilisateur)
 );
 
+--création de la table voiture --  testé et c'est bon
+
 CREATE TABLE Voiture(
-   Matricule VARCHAR(50),
-   Marque VARCHAR(50),
-   Modèle VARCHAR(50),
-   Type VARCHAR(50),
-   Couleur VARCHAR(50),
-   NbPlace BYTE,
-   Carburant VARCHAR(50),
+   Matricule VARCHAR(50) CHECK (Matricule ~ '^[A-Z0-9]+$'),
+   Marque VARCHAR(50) NOT NULL CHECK (Marque ~ '^[A-Za-z0-9 ]+$'),
+   Modele VARCHAR(50) NOT NULL CHECK (Modele ~ '^[A-Za-z0-9 ]+$'),
+   Type VARCHAR(50) NOT NULL CHECK (Type IN ('SUV', 'Berline', 'Compacte', 'Monospace')) ,
+   Couleur VARCHAR(50)  NOT NULL CHECK (Couleur IN ('bleu', 'violet', 'rose', 'rouge', 'orange', 'jaune', 'vert', 'noir', 'marron', 'gris', 'aluminium', 'argent', 'blanc')),
+   NbrPlace INT NOT NULL CHECK (NbrPlace > 0 AND NbrPlace <= 100),
+   Carburant VARCHAR(50) NOT NULL CHECK (Carburant IN ('essence', 'diesel', 'éthanol', 'gaz', 'électrique', 'hybride')),
    NumPermis VARCHAR(50) NOT NULL,
    PRIMARY KEY(Matricule),
    FOREIGN KEY(NumPermis) REFERENCES Conducteur(NumPermis)
 );
+--création de la table trajet  --  testé et c'est bon
 
 CREATE TABLE Trajet(
    IdTrajet INT,
-   VilleDepart VARCHAR(50),
-   CodePostalDepart VARCHAR(50),
-   NomRueDepart VARCHAR(50),
-   NumRueDepart VARCHAR(50),
-   VilleArrivée VARCHAR(50),
-   CodePostalArrivée VARCHAR(50),
-   NomRueArrivée VARCHAR(50),
-   NumRueArrivée VARCHAR(50),
-   CommentaireTrajetConducteur VARCHAR(50),
-   PlaceDispo BYTE,
+   VilleDepart VARCHAR(100)NOT NULL CHECK (VilleDepart ~ '^[A-Za-z ]+$'),
+   CodePostalDepart VARCHAR(10) NOT NULL CHECK (CodePostalDepart ~ '^\d{5}$'),
+   NomRueDepart VARCHAR(50) NOT NULL CHECK (NomRueDepart ~ '^[A-Za-z ]+$'),
+   NumRueDepart INT NOT NULL CHECK (NumRueDepart BETWEEN 1 AND 9999),
+   VilleArrivee VARCHAR(100) NOT NULL CHECK (VilleArrivee ~ '^[A-Za-z ]+$'),
+   CodePostalArrivee VARCHAR(5) NOT NULL CHECK (CodePostalArrivee ~ '^\d{5}$'),
+   NomRueArrivee VARCHAR(50) NOT NULL CHECK (NomRueArrivee ~ '^[A-Za-z ]+$'),
+   NumRueArrivee INT NOT NULL CHECK (NumRueArrivee BETWEEN 1 AND 9999),
+   CommentaireTrajetConducteur TEXT,
+   PlaceDispo INT CHECK (PlaceDispo > 0 ),
    Matricule VARCHAR(50) NOT NULL,
    NumPermis VARCHAR(50) NOT NULL,
    PRIMARY KEY(IdTrajet),
    FOREIGN KEY(Matricule) REFERENCES Voiture(Matricule),
    FOREIGN KEY(NumPermis) REFERENCES Conducteur(NumPermis)
 );
+--création de la table Reservation  --  testé et c'est bon
 
 CREATE TABLE Reservation(
    IdRes INT,
@@ -128,42 +148,46 @@ CREATE TABLE Reservation(
    FOREIGN KEY(NumPermis) REFERENCES Conducteur(NumPermis),
    FOREIGN KEY(IdTrajet) REFERENCES Trajet(IdTrajet)
 );
+--création de la table envoyer  --  testé et c'est bon
 
 CREATE TABLE Envoyer(
    IdSession INT,
    IdUtilisateur INT,
-   Jour BYTE,
-   Mois SMALLINT,
-   Annee INT,
-   PRIMARY KEY(IdSession, IdUtilisateur, Jour, Mois, Annee),
+   Jour INT CHECK (Jour BETWEEN 1 AND 31) NOT NULL,
+    Mois INT CHECK (Mois BETWEEN 1 AND 12) NOT NULL,
+    Annee INT CHECK (Annee > 0) NOT NULL,
+    PRIMARY KEY(IdSession, IdUtilisateur, Jour, Mois, Annee),
    FOREIGN KEY(IdSession) REFERENCES Messagerie(IdSession),
    FOREIGN KEY(IdUtilisateur) REFERENCES Utilisateur(IdUtilisateur),
    FOREIGN KEY(Jour, Mois, Annee) REFERENCES LaDate(Jour, Mois, Annee)
 );
+--création de la table recevoir  --  testé et c'est bon
 
 CREATE TABLE Recevoir(
    IdSession INT,
    IdUtilisateur INT,
-   Jour BYTE,
-   Mois SMALLINT,
-   Annee INT,
+   Jour INT CHECK (Jour BETWEEN 1 AND 31) NOT NULL,
+    Mois INT CHECK (Mois BETWEEN 1 AND 12) NOT NULL,
+    Annee INT CHECK (Annee > 0) NOT NULL,
    PRIMARY KEY(IdSession, IdUtilisateur, Jour, Mois, Annee),
    FOREIGN KEY(IdSession) REFERENCES Messagerie(IdSession),
    FOREIGN KEY(IdUtilisateur) REFERENCES Utilisateur(IdUtilisateur),
    FOREIGN KEY(Jour, Mois, Annee) REFERENCES LaDate(Jour, Mois, Annee)
 );
+--création de la table supprimer  --  testé et c'est bon
 
 CREATE TABLE Supprimer(
    IdUtilisateur INT,
    IdAdm INT,
-   Jour BYTE,
-   Mois SMALLINT,
-   Annee INT,
+   Jour INT CHECK (Jour BETWEEN 1 AND 31) NOT NULL,
+    Mois INT CHECK (Mois BETWEEN 1 AND 12) NOT NULL,
+    Annee INT CHECK (Annee > 0) NOT NULL,
    PRIMARY KEY(IdUtilisateur, IdAdm, Jour, Mois, Annee),
    FOREIGN KEY(IdUtilisateur) REFERENCES Utilisateur(IdUtilisateur),
    FOREIGN KEY(IdAdm) REFERENCES Administrateur(IdAdm),
    FOREIGN KEY(Jour, Mois, Annee) REFERENCES LaDate(Jour, Mois, Annee)
 );
+--création de la table reserver  --  testé et c'est bon
 
 CREATE TABLE Reserver(
    IdUtilisateur INT,
@@ -172,6 +196,7 @@ CREATE TABLE Reserver(
    FOREIGN KEY(IdUtilisateur) REFERENCES Utilisateur(IdUtilisateur),
    FOREIGN KEY(IdRes) REFERENCES Reservation(IdRes)
 );
+--création de la table annulerR  --  testé et c'est bon
 
 CREATE TABLE AnnulerR(
    IdUtilisateur INT,
@@ -180,7 +205,7 @@ CREATE TABLE AnnulerR(
    FOREIGN KEY(IdUtilisateur) REFERENCES Utilisateur(IdUtilisateur),
    FOREIGN KEY(IdRes) REFERENCES Reservation(IdRes)
 );
-
+--création de la table rerchercher  --  testé et c'est bon
 CREATE TABLE Rechercher(
    IdUtilisateur INT,
    IdTemp VARCHAR(50),
@@ -190,53 +215,62 @@ CREATE TABLE Rechercher(
    FOREIGN KEY(IdTemp) REFERENCES Visiteur(IdTemp),
    FOREIGN KEY(IdTrajet) REFERENCES Trajet(IdTrajet)
 );
+--création de la table départ   --  testé et c'est bon
 
 CREATE TABLE Départ(
    IdTrajet INT,
+
+   JourDepart INT NOT NULL CHECK (JourDepart BETWEEN 1 AND 7),
+    JourArrivee INT NOT NULL CHECK (JourArrivee BETWEEN 1 AND 7),
+    Semaine INT NOT NULL CHECK (Semaine BETWEEN 1 AND 52),
    HeureDepart TIME,
-   JourDépart DATE,
-   JourArrivée DATE,
-   Semaine INT,
    HeureArrivee TIME,
-   PRIMARY KEY(IdTrajet, HeureDepart, JourDépart, JourArrivée, Semaine),
+   
+   PRIMARY KEY(IdTrajet, HeureDepart, JourDepart, JourArrivee, Semaine),
    FOREIGN KEY(IdTrajet) REFERENCES Trajet(IdTrajet),
    FOREIGN KEY(HeureDepart) REFERENCES Calendrier(HeureDepart),
-   FOREIGN KEY(JourDépart, JourArrivée, Semaine) REFERENCES Jour(JourDépart, JourArrivée, Semaine)
+   FOREIGN KEY(JourDepart, JourArrivee, Semaine) REFERENCES Jour(JourDepart, JourArrivee, Semaine)
 );
 
+--création de la table CommenterReservation   --  testé et c'est bon
 CREATE TABLE CommenterReservation(
    IdUtilisateur INT,
    IdRes INT,
-   CommentairePassager VARCHAR(50),
-   NoteTrajet BYTE,
+   CommentairePassager TEXT,
+   NoteTrajet INT CHECK (NoteTrajet BETWEEN 0 AND 5),
    PRIMARY KEY(IdUtilisateur, IdRes),
    FOREIGN KEY(IdUtilisateur) REFERENCES Utilisateur(IdUtilisateur),
    FOREIGN KEY(IdRes) REFERENCES Reservation(IdRes)
 );
 
+--création de la table MettreJour--  testé et c'est bon
+
 CREATE TABLE MettreJour(
    IdUtilisateur INT,
    IdUtilisateur_1 INT,
-   Jour BYTE,
-   Mois SMALLINT,
-   Annee INT,
+   Jour INT CHECK (Jour BETWEEN 1 AND 31) NOT NULL,
+    Mois INT CHECK (Mois BETWEEN 1 AND 12) NOT NULL,
+    Annee INT CHECK (Annee > 0) NOT NULL,
    PRIMARY KEY(IdUtilisateur, IdUtilisateur_1, Jour, Mois, Annee),
    FOREIGN KEY(IdUtilisateur) REFERENCES Utilisateur(IdUtilisateur),
    FOREIGN KEY(IdUtilisateur_1) REFERENCES Utilisateur(IdUtilisateur),
    FOREIGN KEY(Jour, Mois, Annee) REFERENCES LaDate(Jour, Mois, Annee)
 );
 
+--création de la table Interdire --  testé et c'est bon
+
 CREATE TABLE Interdire(
    IdAdm INT,
-   Jour BYTE,
-   Mois SMALLINT,
-   Annee INT,
+   Jour INT CHECK (Jour BETWEEN 1 AND 31) NOT NULL,
+    Mois INT CHECK (Mois BETWEEN 1 AND 12) NOT NULL,
+    Annee INT CHECK (Annee > 0) NOT NULL,
    NumPermis VARCHAR(50),
    PRIMARY KEY(IdAdm, Jour, Mois, Annee, NumPermis),
    FOREIGN KEY(IdAdm) REFERENCES Administrateur(IdAdm),
    FOREIGN KEY(Jour, Mois, Annee) REFERENCES LaDate(Jour, Mois, Annee),
    FOREIGN KEY(NumPermis) REFERENCES Conducteur(NumPermis)
 );
+--création de la table AnnulerT --  testé et c'est bon
 
 CREATE TABLE AnnulerT(
    IdTrajet INT,
@@ -245,6 +279,7 @@ CREATE TABLE AnnulerT(
    FOREIGN KEY(IdTrajet) REFERENCES Trajet(IdTrajet),
    FOREIGN KEY(NumPermis) REFERENCES Conducteur(NumPermis)
 );
+--création de la table seConnecter --  testé et c'est bon
 
 CREATE TABLE SeConnecter(
    IdUtilisateur INT,
@@ -253,14 +288,17 @@ CREATE TABLE SeConnecter(
    FOREIGN KEY(IdUtilisateur) REFERENCES Utilisateur(IdUtilisateur),
    FOREIGN KEY(IdTemp) REFERENCES Visiteur(IdTemp)
 );
+--création de la table acheter --  testé et c'est bon
 
 CREATE TABLE Acheter(
    NumPermis VARCHAR(50),
-   IdCadeau VARCHAR(50),
+   IdCadeau INT,
    PRIMARY KEY(NumPermis, IdCadeau),
    FOREIGN KEY(NumPermis) REFERENCES Conducteur(NumPermis),
    FOREIGN KEY(IdCadeau) REFERENCES Boutique(IdCadeau)
 );
+--création de la table Contient --  testé et c'est bon
+
 
 CREATE TABLE Contient(
    IdTrajet INT,
@@ -269,73 +307,59 @@ CREATE TABLE Contient(
    FOREIGN KEY(IdTrajet) REFERENCES Trajet(IdTrajet),
    FOREIGN KEY(IdLieu) REFERENCES Escale(IdLieu)
 );
+--création de la table Consulter --  testé et c'est bon
 
 CREATE TABLE Consulter(
    NumPermis VARCHAR(50),
-   IdCadeau VARCHAR(50),
+   IdCadeau INT,
    PRIMARY KEY(NumPermis, IdCadeau),
    FOREIGN KEY(NumPermis) REFERENCES Conducteur(NumPermis),
    FOREIGN KEY(IdCadeau) REFERENCES Boutique(IdCadeau)
 );
+--création de la table CcommenterPassager --  testé et c'est bon
 
 CREATE TABLE CommenterPassager(
    IdRes INT,
    NumPermis VARCHAR(50),
-   CommentaireConducteur VARCHAR(50),
-   NoteReservation VARCHAR(50),
+    CommentaireConducteur TEXT,
+    NoteReservation INT,
    PRIMARY KEY(IdRes, NumPermis),
    FOREIGN KEY(IdRes) REFERENCES Reservation(IdRes),
    FOREIGN KEY(NumPermis) REFERENCES Conducteur(NumPermis)
 );
-
+--création de la table Voir --  testé et c'est bon
 CREATE TABLE Voir(
    IdUtilisateur INT,
-   IdHistorique VARCHAR(50),
+   IdHistorique INT,
    PRIMARY KEY(IdUtilisateur, IdHistorique),
    FOREIGN KEY(IdUtilisateur) REFERENCES Utilisateur(IdUtilisateur),
    FOREIGN KEY(IdHistorique) REFERENCES Historique(IdHistorique)
 );
 
-"""Insertion"""
 
-INSERT INTO Utilisateur (IdUtilisateur, Mail, MotDePasse, Nom, Prenom, NotePassager, Tel, Sexe, Langue1, Langue2, Handicap, Fumeur)
+--insertion -- 
+
+INSERT INTO LaDate (Jour, Mois, Annee)
 VALUES
-(302, 'marieChimie@gmail.com', 'mARie66:', 'Curie', 'Marie', 4, '06368945', 'F', 'Russe', 'Anglais', FALSE, FALSE),
-(303, 'nizarslamas@gmail.com', 'Nizar7!', 'Slama', 'Nizar', 5, '04875210', 'M', 'Arabe', 'Italien', FALSE, FALSE);
+(1, 1, 1990),
+(2, 2, 1992);
+ 
 
--- Insertion dans la table Rechercher
-INSERT INTO Rechercher (IdUtilisateur, IdTemp, IDTrajet) VALUES
-(901, 101, 801),
-(902, 103, 802),
-(903, 107, 803);
-
--- Insertion dans la table Depart
-INSERT INTO Depart (IdTrajet, JourDepart, JourArrivee, Semaine) VALUES
-(901, 10, 10, 15),
-(902, 22, 23, 20),
-(903, 18, 19, 8);
-
--- Insertion dans la table CommenterReservation
-INSERT INTO CommenterReservation (IdReservation, IdUtilisateur, CommentairePassager, NoteTrajet) VALUES
-(901, 305, 'La conduite était un peu rapide à mon goût.', 3),
-(956, 322, 'Conducteur sympathique et voiture confortable.', 4),
-(906, 365, 'Le trajet était confortable, merci.', 5);
-
--- Insertion dans la table Envoyer
-INSERT INTO Envoyer (IdUtilisateur, IdSession, Jour, Mois, Annee) VALUES
-(358, 901, 10, 10, 15),
-(399, 902, 22, 23, 20),
-(321, 903, 18, 19, 8);
-
-INSERT INTO Trajet (IdTrajet, VilleDepart, CodePostalDepart, NomRueDepart, NumRueDepart, VilleArrivee, CodePostalArrivee, NomRueArrivee, CommentaireTrajetConducteur, PlaceDispo)
+INSERT INTO Utilisateur (IdUtilisateur, Mail, MotDePasse, Nom, Prenom, Sexe, Tel, Handicap, NotePassager, LangueParle1, LangueParle2, Fumeur, Jour, Mois, Annee)
 VALUES
-(801, 'Paris', '75001', 'Rue de Rivoli', 58, 'Lyon', '69002', 'Rue de la Republique', 'Départ matinal, covoiturage convivial', 3),
-(802, 'Marseille', '13002', 'Quai du Port', 17, 'Nice', '06000', 'Ave Jean Medecin', 'Préférence non-fumeur, déplacement professionnel', 2),
-(803, 'Lille', '59000', 'Rue de Paris', 22, 'Bordeaux', '33000', 'Ave Jean Medecin', 'Escale possible à Tours, pas d''animaux', 4);
-
-INSERT INTO Voiture (Matricule, Marque, Modele, Type, Couleur, NbrPlace, Carburant, IdConducteur) VALUES
-('AB123CD', 'Renault', 'Clio', 'Berline', 'Rouge', 5, 'Essence', (SELECT IdConducteur FROM Conducteur WHERE ... condition pour sélectionner le bon conducteur ...)),
-('EF456GH', 'Peugeot', '208', 'Compacte', 'Silver', 4, 'Diesel', (SELECT IdConducteur FROM Conducteur WHERE ... condition pour sélectionner le bon conducteur ...)),
-('IJ789KL', 'Citroen', 'C3', 'SUV', 'Aluminum', 6, 'Hybride', (SELECT IdConducteur FROM Conducteur WHERE ... condition pour sélectionner le bon conducteur ...));
+(302, 'marieChimie@gmail.com', 'mARie66:', 'Curie', 'Marie', 'F', '06368945', FALSE, 4, 'Russe', 'Anglais', FALSE, 1, 1, 1990),
+(303, 'nizarslamas@gmail.com', 'Nizar7!', 'Slama', 'Nizar', 'M', '04875210', FALSE, 5, 'Arabe', 'Italien', FALSE, 2, 2, 1992);
 
 
+INSERT INTO Conducteur (NumPermis, Points, NoteConducteur, IdUtilisateur) VALUES
+('PERMIS123', 12, 4.5, 302), 
+('PERMIS456', 15, 4.7, 303); 
+
+INSERT INTO Voiture (Matricule, Marque, Modele, Type, NbrPlace, Carburant, Couleur, NumPermis) VALUES
+('AB123CD', 'Renault', 'Clio', 'Berline', 5, 'essence', 'rouge', 'PERMIS123'),
+('EF456GH', 'Peugeot', '208', 'Compacte', 4, 'diesel', 'aluminium', 'PERMIS456');
+
+
+INSERT INTO Trajet (IdTrajet, VilleDepart, CodePostalDepart, NomRueDepart, NumRueDepart, VilleArrivee, CodePostalArrivee, NomRueArrivee, NumRueArrivee, CommentaireTrajetConducteur, PlaceDispo, Matricule, NumPermis) VALUES
+(801, 'Paris', '75001', 'Rue de Rivoli', 58, 'Lyon', '69002', 'Rue de la Republique', 1, 'Départ matinal, covoiturage convivial', 3, 'AB123CD', 'PERMIS123'),
+(802, 'Marseille', '13002', 'Quai du Port', 17, 'Nice', '06000', 'Ave Jean Medecin', 2, 'Préférence non-fumeur, déplacement professionnel', 2, 'EF456GH', 'PERMIS456')
